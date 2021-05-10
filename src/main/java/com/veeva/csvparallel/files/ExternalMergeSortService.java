13257dd2071e69ad6acc4652a9d0505df9529cdd
yossi78 <yossi78@gmail.com>
2021-05-10 17:49:18 +0300

    fix

--- src/main/java/com/veeva/csvparallel/files/ExternalMergeSortService.java
+++ src/main/java/com/veeva/csvparallel/files/ExternalMergeSortService.java
@@ -42,8 +42,7 @@ public class ExternalMergeSortService {
         String columnsLine=br.readLine();
         Integer numberOfLines= FileUtil.getNumberOfLinesFromFile(filePath);
         IntStream fileIndexStream=IntStream.range(0,numberOfLines/maxLineRead);
-//        fileIndexStream.parallel().forEach(c->{
-        fileIndexStream.forEach(c->{
+        fileIndexStream.parallel().forEach(c->{
             try {
                 readChunkOfLineSortAndWriteItToTempFile(br,fileIndex,maxLineRead,compareIndex,filePath);
             } catch (IOException | InterruptedException e) {
@@ -91,34 +90,20 @@ public class ExternalMergeSortService {
 
 
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-
-
-
     //  THE METHOD READ LINES FROM ALL CHUNK TEMP FILES AND SORT THEM AND CREATE FINAL CSV FILE
     private void mergeFiles(int numOfFiles, String columnsLine,int numberOfLines,String filePath) throws IOException, InterruptedException {
         BufferedWriter bwFinal=new BufferedWriter(new FileWriter(filePath+"_sorted.csv"));
         Product smallestProduct= FileUtil.fetchSmallestLineAndRemoveIt(numberOfLines,maxLineRead,filePath,compareIndex);
+        bwFinal.append(columnsLine);
         bwFinal.append(smallestProduct+"\n");
         while (true){
             int fileIndex=smallestProduct.getIndexForFileName();
-            readNextLineComingAfterCurrentOneAndRemoveIt(filePath,fileIndex,numOfFiles,numberOfLines);
+            readNextLineComingAfterCurrentOneAndMoveItToCacheFile(filePath,fileIndex,numOfFiles,numberOfLines);
             smallestProduct=FileUtil.readSmallestJsonFromFileAndRemoveIt(filePath+"_cache",maxLineRead);
             if(smallestProduct==null){
                 break;
             }
-            bwFinal.append(smallestProduct+"\n");
+            bwFinal.append(smallestProduct.getLine()+"\n");
         }
         bwFinal.close();
     }
@@ -144,7 +129,7 @@ public class ExternalMergeSortService {
 
 
 
-    private void readNextLineComingAfterCurrentOneAndRemoveIt(String filePath, int fileIndex, int numOfFiles, int numberOfLines) throws IOException, InterruptedException {
+    private void readNextLineComingAfterCurrentOneAndMoveItToCacheFile(String filePath, int fileIndex, int numOfFiles, int numberOfLines) throws IOException, InterruptedException {
         Product product=null;
         product = FileUtil.readLineAndRemoveIt(filePath+CHUNK+fileIndex,fileIndex,maxLineRead,compareIndex);
         if(product==null) {
